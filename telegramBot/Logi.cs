6db427b58cc4508df01efd853c4ea0db0c516418
Sellzxc1 <zaki94247@gmail.com>
2026-01-28 20:38:01 +0600

    telegram Bot с Логами ветками

---
+++ telegramBot/Logi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telegramBot
+{
+    internal class Logi
+    {
+        public static void Record(UserLog userLog, string File_logi, string file)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(UserLog));
+
+
+            if (!Directory.Exists(File_logi))
+            {
+                Directory.CreateDirectory(File_logi); 
+            }
+
+            using (var writeStream = System.IO.File.Open(file, FileMode.Append, FileAccess.Write))
+            {
+
+                serializer.WriteObject(writeStream, userLog);
+
+                writeStream.WriteByte((byte)'\n');
+            }
+        }
+    }
+}
---
+++ telegramBot/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using Telegram.BotAPI;
+using Telegram.BotAPI.AvailableMethods;
+using Telegram.BotAPI.AvailableTypes;
+using Telegram.BotAPI.GettingUpdates;
+using telegramBot;
+
+
+[DataContract]
+public class UserLog
+{
+    [DataMember]
+    public string Times { get; set; }
+
+    [DataMember]
+    public required string UserName { get; set; }
+
+    [DataMember]
+    public long UserId { get; set; }
+
+    [DataMember]
+    public string Message_Text { get; set; }
+
+}
+class Program
+{
+   
+
+    static void Main()
+    {
+
+        string token = "8061818030:AAHxPjLwvLtOsTxRb4gi0xATfq1j8geDXJo";
+
+        var bot = new TelegramBotClient(token);
+        Console.WriteLine("Бот запущен!");
+
+        int offset = 0;
+
+        while (true)
+        {
+            try
+            {
+                var updates = bot.GetUpdates(offset);
+
+                foreach (var update in updates)
+                {
+                    offset = update.UpdateId + 1;
+
+                    string ID = update.Message.From.Id.ToString(); ;
+
+                    string File_logi = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logi");
+                    string file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logi", $"{ID}.json");
+
+
+
+                    if (update.Message?.Text != null)
+                    {
+                        UserLog userLog = new UserLog
+                        {
+                            Times = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"),
+                            UserId = update.Message.From.Id,
+                            UserName = update.Message.From.Username ?? "Неизвестно",
+                            Message_Text = update.Message.Text
+                        };
+
+                        Logi.Record(userLog, File_logi, file);
+                       
+                        string userMessage = update.Message.Text.ToLower();
+
+                        if (userMessage == "/start")
+                        {
+                            bot.SendMessage(
+                                update.Message.Chat.Id,
+                                "Бот запущен"
+                            );
+                        }
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка: {e.Message}");
+            }
+        }
+    }
+}
